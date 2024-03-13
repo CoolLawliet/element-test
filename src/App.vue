b689df8de694dbd1c8bed98c5bb681558205df63
lihuike <lihuike@dhgate.com>
2024-03-13 13:53:10 +0800

    fix: vite

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    {{ message }}
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'app',
+  data() {
+    return {
+      message: 'hello vite-vue2'
+    }
+  }
+}
+</script>
+
+<style></style>
+
